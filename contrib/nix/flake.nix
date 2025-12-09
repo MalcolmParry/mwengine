356783d947d0258f2655fc32f05e992f2d04cfea
Malcolm Parry <malckinator@gmail.com>
2025-12-09 18:47:43 +0000

    added nix dev shell

---
+++ contrib/nix/flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "mwengine build dev shell";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      flake-utils,
+      nixpkgs,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.zig_0_15
+            pkgs.libx11
+            pkgs.vulkan-loader
+          ];
+
+          shellHook = ''
+            echo Entering Dev Shell
+            echo Zig Version: $(zig version)
+            echo ""
+          '';
+        };
+
+      }
+    );
+}
