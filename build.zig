ce834606048e700bdde01a9827f34949c6d5f46a
Malcolm Parry <malckinator@gmail.com>
2026-01-25 01:17:30 +0000

    removed x11 from build script

--- build.zig
+++ build.zig
@@ -6,17 +6,11 @@ pub fn build(b: *Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const use_glfw = b.option(bool, "glfw", "Use glfw for window") orelse true;
-
     const module = b.addModule("mwengine", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
-
-    const options = b.addOptions();
-    options.addOption(bool, "use_glfw", use_glfw);
-    module.addOptions("build_options", options);
 
     const vulkan = b.dependency("vulkan", .{
         .registry = b.dependency("vulkan_headers", .{}).path("registry/vk.xml"),
@@ -25,21 +19,16 @@ pub fn build(b: *Build) !void {
     }).module("vulkan-zig");
     module.addImport("vulkan", vulkan);
 
-    if (use_glfw) {
-        const glfw = b.dependency("zglfw", .{
-            .target = target,
-            .optimize = optimize,
-            .import_vulkan = true,
-        });
-        const glfw_mod = glfw.module("root");
-        glfw_mod.addImport("vulkan", vulkan);
+    const glfw = b.dependency("zglfw", .{
+        .target = target,
+        .optimize = optimize,
+        .import_vulkan = true,
+    });
+    const glfw_mod = glfw.module("root");
+    glfw_mod.addImport("vulkan", vulkan);
 
-        module.addImport("glfw", glfw_mod);
-        module.linkLibrary(glfw.artifact("glfw"));
-    } else {
-        module.link_libc = true;
-        module.linkSystemLibrary("X11", .{});
-    }
+    module.addImport("glfw", glfw_mod);
+    module.linkLibrary(glfw.artifact("glfw"));
 
     // test
     const unit_tests = b.addTest(.{
