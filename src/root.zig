60438f1f45931c5564b5af89a7a78678cb997a8e
Malcolm Parry <me@mparry.dev>
2026-04-24 01:25:02 +0100

    removed debug renderer

--- src/root.zig
+++ src/root.zig
@@ -5,7 +5,6 @@ pub const Event = @import("events.zig").Event;
 pub const EventQueue = @import("events.zig").Queue;
 pub const math = @import("math.zig");
 pub const gpu = @import("gpu/gpu.zig");
-pub const DebugRenderer = @import("DebugRenderer.zig");
 pub const text = @import("text.zig");
 pub const ImmediateRenderer = @import("renderer/Immediate.zig");
 
